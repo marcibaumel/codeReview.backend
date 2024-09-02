028b4ca8ada3d73d6c200dde79c8656e7d621bd3
Marci <marcibaumel@gmail.com>
2024-09-02 20:46:50 +0200

    CRUD app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
+import { ItemsModule } from './items/items.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule, ItemsModule],
 })
-export class AppModule {}
+export class AppModule { }
